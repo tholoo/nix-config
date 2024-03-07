bfd8ff7025de67a83bd0af227bc1df53c8c5c210
Ali M <ali.mohamadza@gmail.com>
2024-03-07 19:10:40 +0330

    lf and mpv

---
+++ home-manager/programs/mpv/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }: {
+  programs.mpv = {
+    enable = true;
+    config = {
+      profile = "gpu-hq";
+      ytdl-format = "bestvideo+bestaudio";
+      # cache-default = 4000000;
+    };
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      # Feature-rich minimalist proximity-based UI for MPV player
+      uosc
+      seekTo
+      thumbnail
+      thumbfast
+      mpvacious
+      # Youtube
+      sponserblock
+      quality-menu
+    ];
+  };
+}
