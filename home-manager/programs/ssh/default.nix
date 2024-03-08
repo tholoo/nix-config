aa87808b3f61ae64ad468f36a5592fae0abef1f3
Ali M <ali.mohamadza@gmail.com>
2024-03-08 15:11:49 +0330

    qol ssh for github

--- home-manager/programs/ssh/default.nix
+++ home-manager/programs/ssh/default.nix
@@ -2,6 +2,12 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
+    matchBlocks = {
+      "github" = {
+        hostname = "github.com";
+        user = "git";
+      };
+    };
   };
   services.ssh-agent.enable = true;
 }
