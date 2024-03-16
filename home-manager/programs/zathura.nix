95ba7593016f82251b4b34bb6a5ddd082f2ae9d0
Ali M <ali.mohamadza@gmail.com>
2024-03-17 00:24:26 +0330

    add zathura

---
+++ home-manager/programs/zathura.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  programs.zathura = {
+    enable = true;
+    options = {
+      statusbar-h-padding = 0;
+      statusbar-v-padding = 0;
+      page-padding = 1;
+    };
+    mappings = {
+      u = "scroll half-up";
+      d = "scroll half-down";
+      D = "toggle_page_mode";
+      r = "reload";
+      R = "rotate";
+    };
+  };
+}
