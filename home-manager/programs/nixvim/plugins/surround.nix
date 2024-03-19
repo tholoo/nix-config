02f53355e95c30ac9a3b5da923c9ee7a3a6c923e
Ali M <ali.mohamadza@gmail.com>
2024-03-19 14:31:58 +0330

    add better surround

--- home-manager/programs/nixvim/plugins/surround.nix
+++ home-manager/programs/nixvim/plugins/surround.nix
@@ -1,1 +1,14 @@
-{ surround.enable = true; }
+{ pkgs, ... }: {
+  plugins = { };
+  extraPlugins = with pkgs.vimPlugins; [ nvim-surround ];
+  extraConfigLua = ''
+    require("nvim-surround").setup({
+      keymaps = {
+        normal = "s",
+        normal_cur = "ss",
+        visual = "s",
+        visual_line = "gs",
+      },
+    })
+  '';
+}
