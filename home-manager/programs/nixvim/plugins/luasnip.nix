6d7e1b317b89bad8519f269ab29a4d2ffa36e411
Ali M <ali.mohamadza@gmail.com>
2024-03-25 00:14:03 +0330

    add luasnip keybindings

--- home-manager/programs/nixvim/plugins/luasnip.nix
+++ home-manager/programs/nixvim/plugins/luasnip.nix
@@ -1,1 +1,45 @@
-{ luasnip.enable = true; }
+{ lib, ... }: {
+  plugins = {
+    luasnip.enable = true;
+    friendly-snippets.enable = true;
+  };
+
+  keymaps = lib.mkAfter [
+    {
+      key = "<C-l>";
+      action = "function() require('luasnip').jump(1) end";
+      lua = true;
+      mode = [ "i" "s" ];
+      options = {
+        silent = true;
+        noremap = true;
+      };
+    }
+    {
+      key = "<C-h>";
+      action = "function() require('luasnip').jump(-1) end";
+      lua = true;
+      mode = [ "i" "s" ];
+      options = {
+        silent = true;
+        noremap = true;
+      };
+    }
+    {
+      key = "<C-E>";
+      action = ''
+        function()
+          if require('luasnip').choice_active() then
+              require('luasnip').change_choice(1)
+          end
+        end
+      '';
+      lua = true;
+      mode = [ "i" "s" ];
+      options = {
+        silent = true;
+        noremap = true;
+      };
+    }
+  ];
+}
