a84be7521217f42165559f91691705a3fbdf1692
Ali M <ali.mohamadza@gmail.com>
2024-03-25 00:14:05 +0330

    add neogen

--- home-manager/programs/nixvim/plugins/neogen.nix
+++ home-manager/programs/nixvim/plugins/neogen.nix
@@ -1,6 +1,29 @@
-{
-  neogen = {
+{ lib, ... }: {
+  plugins.neogen = {
     enable = true;
     snippetEngine = "luasnip";
   };
+
+  # keymaps = lib.mkAfter [
+  #   {
+  #     key = "<C-l>";
+  #     action = "function() require('neogen').jump_next end";
+  #     lua = true;
+  #     mode = [ "i" "s" ];
+  #     options = {
+  #       silent = true;
+  #       noremap = true;
+  #     };
+  #   }
+  #   {
+  #     key = "<C-h>";
+  #     action = "function() require('neogen').jump_prev end";
+  #     lua = true;
+  #     mode = [ "i" "s" ];
+  #     options = {
+  #       silent = true;
+  #       noremap = true;
+  #     };
+  #   }
+  # ];
 }
