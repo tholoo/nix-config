74d681f2df8bd359ce8b7f794a5d1c216af0c293
Ali M <ali.mohamadza@gmail.com>
2024-03-24 20:37:27 +0330

    try nvim symlinking

--- home-manager/programs/nvim.nix
+++ home-manager/programs/nvim.nix
@@ -1,18 +1,18 @@
-{ ... }: {
+{ pkgs, config, ... }: {
+  # config.home.file."./.local/share/nvim/my-local-lazy/nvim-treesitter/" = {
+    # recursive = true;
+    # source = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+  # };
   # programs.neovim = {
     # enable = true;
     # withPython3 = true;
     # withRuby = true;
     # withNodeJs = true;
     # defaultEditor = true;
-    # extraPython3Packages = pyPkgs: with pyPkgs; [
-      # pynvim
-      # python-lsp-ruff
-      # mypy
-    # ];
+    # extraPython3Packages = pyPkgs: with pyPkgs; [ pynvim python-lsp-ruff mypy ];
   # };
   # xdg.configFile.nvim = {
-    # source = ../../resources/nvim;
+    # source = config.lib.file.mkOutOfStoreSymlink "/home/tholo/nix-config/resources/nvim";
     # recursive = true;
   # };
 }
