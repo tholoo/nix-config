087aeae66eb5fd3eb53bb9b46d0d5f92a9ac860d
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-03-26 18:18:32 +0330

    fzf change fd cmd

--- home-manager/programs/fzf.nix
+++ home-manager/programs/fzf.nix
@@ -1,17 +1,18 @@
 { pkgs, lib, ... }: {
   programs.fzf = let
     excludes = lib.fold (el: c: "${c} --exclude ${el}" ) "" ["__pycache__"];
-    tre_cmd = "${pkgs.tre-command}/bin/tre --color always {} --limit 5 --all";
-    bat_cmd = "${pkgs.bat}/bin/bat --color always {}";
+    tre_cmd = "${pkgs.tre-command}/bin/tre --color always {} --limit 5 --all ${excludes}";
+    bat_cmd = "${pkgs.bat}/bin/bat ${excludes} --color always {}";
+    fd_cmd = "${pkgs.fd}/bin/fd --hidden --no-ignore ${excludes}";
   in
   {
     enable = true;
-    defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden";
+    defaultCommand = "${fd_cmd} --type f";
 
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden ${excludes}";
+    changeDirWidgetCommand = "${fd_cmd} --type d";
     changeDirWidgetOptions = [ "--preview '${tre_cmd} | head -100'" ];
 
-    fileWidgetCommand = "${pkgs.fd}/bin/fd --hidden ${excludes}";
+    fileWidgetCommand = fd_cmd;
     fileWidgetOptions = [ "--preview '${bat_cmd} 2> /dev/null || ${tre_cmd} | head -100'" ];
 
     historyWidgetOptions = [ "--reverse" ];
