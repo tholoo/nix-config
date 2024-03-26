cbcd67e39f781b03cd737ebe739bab678eb90c76
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-03-26 17:27:57 +0330

    config fzf

--- home-manager/programs/fzf.nix
+++ home-manager/programs/fzf.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }: {
   programs.fzf = {
     enable = true;
-    enableFishIntegration = true;
+    defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden";
+
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --exclude __pycache__";
+    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --hidden --exclude __pycache__";
+    fileWidgetOptions = [ "--preview '${pkgs.bat}/bin/bat --color always {} 2> /dev/null || ${pkgs.tree}/bin/tree -C {}'" ];
+
+    historyWidgetOptions = [ "--reverse" ];
+
     tmux.enableShellIntegration = false;
   };
 }
