21f3c4ca05745f22a02b1ca3cf717e2721de3836
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-03-26 17:34:24 +0330

    switch from tree to tre

--- home-manager/programs/fzf.nix
+++ home-manager/programs/fzf.nix
@@ -4,10 +4,10 @@
     defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden";
 
     changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --exclude __pycache__";
-    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    changeDirWidgetOptions = [ "--preview '${pkgs.tre-command}/bin/tre -C {} | head -200'" ];
 
     fileWidgetCommand = "${pkgs.fd}/bin/fd --hidden --exclude __pycache__";
-    fileWidgetOptions = [ "--preview '${pkgs.bat}/bin/bat --color always {} 2> /dev/null || ${pkgs.tree}/bin/tree -C {}'" ];
+    fileWidgetOptions = [ "--preview '${pkgs.bat}/bin/bat --color always {} 2> /dev/null || ${pkgs.tre-command}/bin/tre -C {}'" ];
 
     historyWidgetOptions = [ "--reverse" ];
 
