92aa0c2c885584f5177f29470a5d43ee5ab789a3
Ali M <ali.mohamadza@gmail.com>
2024-03-28 01:40:01 +0330

    fix bat in fzf

--- home-manager/programs/fzf.nix
+++ home-manager/programs/fzf.nix
@@ -1,11 +1,11 @@
 { pkgs, lib, ... }: {
   programs.fzf = let
-    excludes = lib.fold (el: c: "${c} --exclude ${el}" ) "" ["__pycache__"];
-    tre_cmd = "${pkgs.tre-command}/bin/tre --color always {} --limit 5 --all ${excludes}";
-    bat_cmd = "${pkgs.bat}/bin/bat ${excludes} --color always {}";
+    excludes = lib.fold (el: c: "${c} --exclude ${el}") "" [ "__pycache__" ];
+    tre_cmd =
+      "${pkgs.tre-command}/bin/tre --color always {} --limit 5 --all ${excludes}";
+    bat_cmd = "${pkgs.bat}/bin/bat --color always {}";
     fd_cmd = "${pkgs.fd}/bin/fd --hidden --no-ignore ${excludes}";
-  in
-  {
+  in {
     enable = true;
     defaultCommand = "${fd_cmd} --type f";
 
@@ -13,7 +13,8 @@
     changeDirWidgetOptions = [ "--preview '${tre_cmd} | head -100'" ];
 
     fileWidgetCommand = fd_cmd;
-    fileWidgetOptions = [ "--preview '${bat_cmd} 2> /dev/null || ${tre_cmd} | head -100'" ];
+    fileWidgetOptions =
+      [ "--preview '${bat_cmd} 2> /dev/null || ${tre_cmd} | head -100'" ];
 
     historyWidgetOptions = [ "--reverse" ];
 
