1afa1638d1e16abac581cb1660d9c2e31cdd6f50
Ali M <ali.mohamadza@gmail.com>
2024-03-28 21:54:51 +0330

    change luasnip keybindings

--- home-manager/programs/nixvim/plugins/luasnip.nix
+++ home-manager/programs/nixvim/plugins/luasnip.nix
@@ -6,17 +6,31 @@
 
   keymaps = lib.mkAfter [
     {
-      key = "<C-l>";
+      key = "<tab>";
+      action = ''
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end
+      '';
+      lua = true;
+      mode = "i";
+      options = {
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      key = "<tab>";
       action = "function() require('luasnip').jump(1) end";
       lua = true;
-      mode = [ "i" "s" ];
+      mode = [ "s" ];
       options = {
         silent = true;
         noremap = true;
       };
     }
     {
-      key = "<C-h>";
+      key = "<s-tab>";
       action = "function() require('luasnip').jump(-1) end";
       lua = true;
       mode = [ "i" "s" ];
