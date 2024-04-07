076be1018163011cce767cf0d8d3964ddc0ef633
Ali M <ali.mohamadza@gmail.com>
2024-04-07 20:37:25 +0330

    update git options

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -8,15 +8,14 @@
       # git_protocol = "ssh";
     };
     gh-dash.enable = true;
+    git-cliff.enable = true;
     git = {
       enable = true;
       userName = "Ali M";
       userEmail = "ali.mohamadza@gmail.com";
       # Install git with all the optional extras
       package = pkgs.gitAndTools.gitFull;
-      aliases = {
-        clone-bare = "!sh ${./git-clone-bare.sh}";
-      };
+      aliases = { clone-bare = "!sh ${./git-clone-bare.sh}"; };
       delta.enable = true;
       # diff-so-fancy.enable = true;
       extraConfig = {
@@ -25,6 +24,10 @@
         pull.rebase = false;
         # Cache git credentials for 15 minutes
         credential.helper = "cache";
+        # always use --update-refs
+        rebase.updateRefs = true;
+        # if branch exists in origin, git worktree add will use that branch instead of creating a new one
+        worktree.guessRemote = true;
       };
     };
   };
