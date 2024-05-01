3ec55730d71f49c73607a0d7ba132d1a517224ef
Ali M <ali.mohamadza@gmail.com>
2024-05-01 19:42:13 +0330

    add some nix settings

--- hosts/home/home.nix
+++ hosts/home/home.nix
@@ -63,6 +63,37 @@
         "root"
         "tholo"
       ];
+      max-jobs = "auto";
+
+      # The maximum number of parallel TCP connections used to fetch files from binary caches and by other downloads.
+      # It defaults to 25. 0 means no limit.
+      http-connections = 128;
+
+      # This option defines the maximum number of substitution jobs that Nix will try to run in
+      # parallel. The default is 16. The minimum value one can choose is 1 and lower values will be
+      # interpreted as 1.
+      max-substitution-jobs = 128;
+
+      # The number of lines of the tail of the log to show if a build fails.
+      log-lines = 25;
+
+      # When free disk space in /nix/store drops below min-free during a build, Nix performs a
+      # garbage-collection until max-free bytes are available or there is no more garbage.
+      # A value of 0 (the default) disables this feature.
+      # min-free = 128000000; # 128 MB
+      # max-free = 1000000000; # 1 GB
+
+      # Prevent garbage collection from altering nix-shells managed by nix-direnv
+      # https://github.com/nix-community/nix-direnv#installation
+      # keep-outputs = true;
+      # keep-derivations = true;
+
+      # Automatically detect files in the store that have identical contents, and replaces
+      # them with hard links to a single copy. This saves disk space.
+      auto-optimise-store = true;
+
+      # Whether to warn about dirty Git/Mercurial trees.
+      # warn-dirty = false;
     };
   };
   nixpkgs = {
