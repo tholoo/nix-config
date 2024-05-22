7c4ed2a23bff550cb442d584b384e0d42186d04f
Ali M <ali.mohamadza@gmail.com>
2024-05-22 13:08:05 +0330

    improve fd

--- home-manager/programs/tui/utils.nix
+++ home-manager/programs/tui/utils.nix
@@ -4,7 +4,6 @@
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
-    fd # A rust alternative to find
     watchexec # Run commands based on file change
     satty # Screen annotatiaon tool
     bottom # System monitor
