a1c05e1681e30722b4495d190450fbfab9bf719e
tholo <ali.mohamadza@gmail.com>
2024-06-15 20:28:32 +0330

    oil show hidden by default

--- modules/home/apps/tui/nixvim/plugins/oil.nix
+++ modules/home/apps/tui/nixvim/plugins/oil.nix
@@ -5,8 +5,19 @@
     # keymaps = { "<leader>i" = "actions.open_cwd"; };
     # keymaps = { callback = "function() require('oil').open_cwd() end", desc = “”, nowait = true })
     settings = {
+      default_file_explorer = true;
       delete_to_trash = true;
       skip_confirm_for_simple_edits = true;
+      view_options = {
+        show_hidden = true;
+        natural_order = true;
+        is_always_hidden = # lua
+          ''
+            function (name, _)
+              return name == ".." or name == ".git" or name == ".direnv"
+            end
+          '';
+      };
     };
   };
   keymaps = lib.mkAfter [
