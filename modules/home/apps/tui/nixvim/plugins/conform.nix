552a1c9edd320c67cc5a9d0de3fe69e72097823d
tholo <ali.mohamadza@gmail.com>
2024-08-31 13:50:49 +0330

    update lock file

--- modules/home/apps/tui/nixvim/plugins/conform.nix
+++ modules/home/apps/tui/nixvim/plugins/conform.nix
@@ -2,46 +2,48 @@
   conform-nvim = {
     enable = false;
     # formatters = {inherit }
-    formatOnSave = {
-      lspFallback = true;
-    };
+    settings = {
+      format_on_save = {
+        lspFallback = true;
+      };
 
-    formattersByFt = {
-      lua = [ "stylua" ];
-      nix = [ ];
-      python = [ "ruff_format" ];
-      # Use a sub-list to run only the first available formatter
-      javascript = [
-        [
+      formatters_by_ft = {
+        lua = [ "stylua" ];
+        nix = [ ];
+        python = [ "ruff_format" ];
+        # Use a sub-list to run only the first available formatter
+        javascript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescript = [
           "prettierd"
           "prettier"
-        ]
-      ];
-      typescript = [
-        "prettierd"
-        "prettier"
-      ];
-      typescriptreact = [ "prettier" ];
+        ];
+        typescriptreact = [ "prettier" ];
 
-      # asm = ["asmfmt"];
-      # c = ["astyle"];
-      # cpp = ["astyle"];
-      # css = ["prettierd" "prettier"];
-      # cmake = ["cmake_format"];
-      # go = ["goimports" "gofumpt" "golines"];
-      # html = ["prettierd" "prettier"];
-      # javascript = ["prettierd" "prettier"];
-      # javascriptreact = ["prettier"];
-      # json = ["prettier"];
-      # markdown = ["prettier"];
-      # rust = ["rustfmt"];
-      # sh = ["shfmt"];
-      # yaml = ["prettierd" "prettier"];
-      # Use the "*" filetype to run formatters on all filetypes.
-      "*" = [ "codespell" ];
-      # Use the "_" filetype to run formatters on filetypes that don't
-      # have other formatters configured.
-      "_" = [ "trim_whitespace" ];
+        # asm = ["asmfmt"];
+        # c = ["astyle"];
+        # cpp = ["astyle"];
+        # css = ["prettierd" "prettier"];
+        # cmake = ["cmake_format"];
+        # go = ["goimports" "gofumpt" "golines"];
+        # html = ["prettierd" "prettier"];
+        # javascript = ["prettierd" "prettier"];
+        # javascriptreact = ["prettier"];
+        # json = ["prettier"];
+        # markdown = ["prettier"];
+        # rust = ["rustfmt"];
+        # sh = ["shfmt"];
+        # yaml = ["prettierd" "prettier"];
+        # Use the "*" filetype to run formatters on all filetypes.
+        "*" = [ "codespell" ];
+        # Use the "_" filetype to run formatters on filetypes that don't
+        # have other formatters configured.
+        "_" = [ "trim_whitespace" ];
+      };
     };
   };
 }
