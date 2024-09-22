e4364404cb347f231a2971cdb309e8626460a046
tholo <ali.mohamadza@gmail.com>
2024-09-22 11:48:35 +0330

    disable useless things on granite to make it lighter

--- homes/x86_64-linux/tholo@granite/default.nix
+++ homes/x86_64-linux/tholo@granite/default.nix
@@ -7,5 +7,14 @@
     };
 
     tui.enable = true;
+    cliphist.enable = false;
+    espanso.enable = false;
+    cli-cool.enable = false;
+    helix.enable = false;
+    kube.enable = false;
+    nixvim.enable = false;
+    nushell.enable = false;
+    proxy.enable = false;
+    translate-shell.enable = false;
   };
 }
