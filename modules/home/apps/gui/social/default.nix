ca159aa8040900713385ee418abe5d48430393a1
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-10-22 22:49:55 +0330

    disable 64gram

--- modules/home/apps/gui/social/default.nix
+++ modules/home/apps/gui/social/default.nix
@@ -17,7 +17,7 @@ in
     home.packages = with pkgs; [
       telegram-desktop
       # since 64gram and telegram-desktop share the same bin name:
-      (pkgs.writeShellScriptBin "64gram" "exec -a $0 ${lib.getExe _64gram} $@")
+      # (pkgs.writeShellScriptBin "64gram" "exec -a $0 ${lib.getExe _64gram} $@")
     ];
   };
 }
