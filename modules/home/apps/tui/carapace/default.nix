4abe7b821f6d7bcc6cffb192e25be5966e7c8630
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-11-01 21:56:12 +0330

    enable carapace completion

---
+++ modules/home/apps/tui/carapace/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "carapace";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "tui"
+      "shell"
+      "interactive"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    programs.${name} = {
+      enable = true;
+    };
+  };
+}
