e4f75fec8136ffb7bff732b3566757716516e9cc
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-12-16 13:16:53 +0330

    add fonts

--- modules/home/apps/gui/fonts/default.nix
+++ modules/home/apps/gui/fonts/default.nix
@@ -25,33 +25,41 @@ in
   };
 
   config = mkIf cfg.enable {
-    fonts.fontconfig.enable = true;
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Liberation Serif"
+          "Vazirmatn"
+        ];
+        sansSerif = [
+          "Ubuntu"
+          "Vazirmatn"
+        ];
+        monospace = [
+          "Ubuntu Mono"
+          "Vazir Code"
+        ];
+      };
+    };
     home.packages =
       with pkgs;
       [
         ubuntu_font_family
+        liberation_ttf
         vazir-fonts # persian font
         vazir-code-font # persian font
         noto-fonts
         # symbola
         codespell
-        noto-fonts
       ]
-      ++ (
-        if cfg.minimal then
-          [
-            (nerdfonts.override {
-              fonts = [
-                "FiraCode"
-                "FiraMono"
-                "JetBrainsMono"
-                "Overpass"
-                "CascadiaCode"
-              ];
-            })
-          ]
-        else
-          [ nerdfonts ]
-      );
+      ++ (with pkgs.nerd-fonts; [
+        fira-code
+        fira-mono
+        jetbrains-mono
+        overpass
+        cascadia-code
+        symbols-only
+      ]);
   };
 }
