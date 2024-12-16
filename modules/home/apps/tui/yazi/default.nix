fb34f45ffaba76bbb9822b597c198c130f9788be
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2024-12-16 13:13:42 +0330

    update yazi

--- modules/home/apps/tui/yazi/default.nix
+++ modules/home/apps/tui/yazi/default.nix
@@ -82,7 +82,7 @@ in
               4
             ];
             show_hidden = true;
-            sort_by = "modified";
+            sort_by = "mtime";
             sort_reverse = true;
             sort_dir_first = true;
             show_symlink = true;
