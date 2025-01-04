01516716f42b040d22c89030e9d10927c30f0646
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-01-04 13:08:41 +0330

    switch from vivaldi to floorp

--- modules/home/apps/gui/hyprland/default.nix
+++ modules/home/apps/gui/hyprland/default.nix
@@ -87,7 +87,7 @@ in
         "$terminal" = lib.getExe pkgs.kitty;
         "$fileManager" = lib.getExe' pkgs.dolphin "dolphin";
         "$menu" = "pgrep wofi >/dev/null 2>&1 || ${lib.getExe pkgs.wofi} --dmenu --show drun";
-        "$browser" = lib.getExe' pkgs.vivaldi "vivaldi";
+        "$browser" = lib.getExe pkgs.floorp;
 
         #################
         ### AUTOSTART ###
