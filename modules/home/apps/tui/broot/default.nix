87c6ce91a14eb1841c92536f885381d3eef4bc85
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-01-07 19:08:41 +0330

    enable broot

---
+++ modules/home/apps/tui/broot/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "broot";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "tui"
+      "shell"
+      "interactive"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    programs.${name} = {
+      enable = true;
+      settings = {
+        modal = true;
+        verbs = [
+          {
+            invocation = "p";
+            execution = ":parent";
+          }
+          {
+            invocation = "edit";
+            shortcut = "e";
+            key = "ctrl-e";
+            apply_to = "text_file";
+            execution = "$EDITOR {file}";
+            leave_broot = "true";
+          }
+        ];
+      };
+    };
+  };
+}
