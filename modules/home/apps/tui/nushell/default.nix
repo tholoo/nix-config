eb60e5cbb9dfbdf6768f53d69bb95d4dbd6e041f
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-01-17 11:31:39 +0330

    fix nushell's extract

--- modules/home/apps/tui/nushell/default.nix
+++ modules/home/apps/tui/nushell/default.nix
@@ -23,7 +23,41 @@ in
       enable = true;
       # envFile.text = ''
       # '';
-      configFile.text = builtins.readFile ./config.nu;
+      configFile.text =
+        builtins.readFile ./config.nu
+        # nu
+        + (
+          with pkgs;
+          with lib;
+          ''
+            export def extract [name:string] {
+              let handlers = [ [extension command];
+                               ['tar\.bz2|tbz|tbz2' '${getExe gnutar} xvjf']
+                               ['tar\.gz|tgz'       '${getExe gnutar} xvzf']
+                               ['tar\.xz|txz'       '${getExe gnutar} xvf']
+                               ['tar\.Z'            '${getExe gnutar} xvZf']
+                               ['bz2'               'bunzip2']
+                               ['deb'               'ar x']
+                               ['gz'                'gunzip']
+                               ['pkg'               'pkgutil --expand']
+                               ['rar'               '${getExe unrar} x']
+                               ['tar'               '${getExe gnutar} xvf']
+                               ['xz'                '${getExe' xz "xz"} --decompress']
+                               ['zip|war|jar|nupkg' '${getExe unzip}']
+                               ['Z'                 'uncompress']
+                               ['7z'                '7za x']
+                             ]
+              let maybe_handler = ($handlers | where $name =~ $'\.(($it.extension))$')
+              if ($maybe_handler | is-empty) {
+                error make { msg: "unsupported file extension" }
+              } else {
+                let handler = ($maybe_handler | first)
+                nu -c ($handler.command + ' ' + $name)
+              }
+            }
+          ''
+        );
+
       shellAliases = {
         e = "env $env.EDITOR";
         f = "${lib.getExe pkgs.yazi}";
