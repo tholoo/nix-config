931f6fc5cea618ddc832770e689a89da2cc5a15c
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-01-27 23:01:14 +0330

    add aria2

---
+++ modules/home/apps/tui/aria2/default.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "aria2";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "tui"
+      "cli-tools"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    programs.aria2 = {
+      enable = true;
+    };
+  };
+}
