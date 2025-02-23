5b95acfa65fd8c0ad36b33480e140c0937e3ae19
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-02-23 20:04:48 +0330

    move obs to nixos instead of home manager

--- modules/home/apps/gui/obs/default.nix
+++ modules/home/apps/gui/obs/default.nix
@@ -21,14 +21,14 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.obs-studio = {
-      enable = true;
-      plugins = with pkgs.obs-studio-plugins; [
-        # Screen capture on wlroots based wayland compositors
-        wlrobs
-        # Audio device and application capture for OBS Studio using PipeWire
-        obs-pipewire-audio-capture
-      ];
-    };
+    # programs.obs-studio = {
+    #   enable = true;
+    #   plugins = with pkgs.obs-studio-plugins; [
+    #     # Screen capture on wlroots based wayland compositors
+    #     wlrobs
+    #     # Audio device and application capture for OBS Studio using PipeWire
+    #     obs-pipewire-audio-capture
+    #   ];
+    # };
   };
 }
