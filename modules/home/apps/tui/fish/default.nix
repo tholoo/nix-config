517132502413c5145bb092d0ea83bedfeb893003
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-03-05 18:24:41 +0330

    disable fish

--- modules/home/apps/tui/fish/default.nix
+++ modules/home/apps/tui/fish/default.nix
@@ -20,7 +20,7 @@ in
 
   config = mkIf cfg.enable {
     programs.fish = {
-      enable = true;
+      enable = false;
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
 
