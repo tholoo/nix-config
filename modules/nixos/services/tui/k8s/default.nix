a8fa31096e440fcf622d655bd18482013a18ba48
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2025-03-29 11:31:18 +0330

    k3s config for flux

--- modules/nixos/services/tui/k8s/default.nix
+++ modules/nixos/services/tui/k8s/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   inherit (lib) mkIf;
   inherit (lib.mine) mkEnable;
@@ -27,8 +32,73 @@ in
       enable = true;
       role = "server";
       extraFlags = toString [
-        # "--debug" # Optionally add additional args to k3s
+        "--node-label \"k3s-upgrade=false\""
+        "--kube-apiserver-arg anonymous-auth=true"
+        "--kube-controller-manager-arg bind-address=0.0.0.0"
+        "--kube-scheduler-arg bind-address=0.0.0.0"
+        "--etcd-expose-metrics"
+        "--secrets-encryption"
+        "--write-kubeconfig-mode 0644"
       ];
+    };
+
+    environment = {
+      systemPackages = with pkgs; [
+        fluxcd
+      ];
+      sessionVariables = {
+        KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+      };
+    };
+
+    systemd = {
+      timers."k3s-flux2-bootstrap" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "3m";
+          OnUnitActiveSec = "3m";
+          Unit = "k3s-flux2-bootstrap.service";
+        };
+      };
+
+      services = {
+        "k3s-flux2-bootstrap" = {
+          script = ''
+            export PATH="$PATH:${pkgs.git}/bin"
+            if ${lib.getExe pkgs.kubectl} get CustomResourceDefinition -A | grep -q "toolkit.fluxcd.io" ; then
+              exit 0
+            fi
+            sleep 30
+            if ${lib.getExe pkgs.kubectl} get CustomResourceDefinition -A | grep -q "toolkit.fluxcd.io" ; then
+              exit 0
+            fi
+            mkdir -p /tmp/k3s-flux2-bootstrap
+            cat > /tmp/k3s-flux2-bootstrap/kustomization.yaml << EOL
+            apiVersion: kustomize.config.k8s.io/v1beta1
+            kind: Kustomization
+            resources:
+              - github.com/fluxcd/flux2/manifests/install
+            patches:
+              # Remove the default network policies
+              - patch: |-
+                  \$patch: delete
+                  apiVersion: networking.k8s.io/v1
+                  kind: NetworkPolicy
+                  metadata:
+                    name: not-used
+                target:
+                  group: networking.k8s.io
+                  kind: NetworkPolicy
+            EOL
+            ${lib.getExe pkgs.kubectl} apply --kustomize /tmp/k3s-flux2-bootstrap
+          '';
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+            RestartSec = "3m";
+          };
+        };
+      };
     };
   };
 }
