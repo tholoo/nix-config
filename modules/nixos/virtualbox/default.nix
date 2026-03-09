b72abce4510c0be133dd53f5c982f3948967695e
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-03-09 20:48:01 +0330

    add vm

---
+++ modules/nixos/virtualbox/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "virtualbox";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "gui"
+      "graphics"
+      "vm"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [ "tholo" ];
+  };
+}
