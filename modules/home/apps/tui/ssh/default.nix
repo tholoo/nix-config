c7eb6da152a44eedf3b6a26ed4263db7d7391848
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-04-13 08:35:16 +0330

    add hosts to ssh

--- modules/home/apps/tui/ssh/default.nix
+++ modules/home/apps/tui/ssh/default.nix
@@ -21,9 +21,7 @@ in
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
-      addKeysToAgent = "yes";
-      serverAliveInterval = 60;
-      serverAliveCountMax = 60;
+      enableDefaultConfig = false;
       matchBlocks =
         let
           cat = lib.getExe' pkgs.coreutils "cat";
@@ -46,11 +44,40 @@ in
             # get the ip from secrets
             proxyCommand = "bash -c '${nc} $(${cat} ${ip-granite.path}) %p'";
           };
-          ahm = {
+          parsa-hetzner-germany = {
+            user = "poweruser";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-parsa-hetzner-germany.path}) %p'";
+          };
+          ahmad-hetzner-germany = {
             user = "root";
             checkHostIP = false;
-            # get the ip from secrets. first line is ip and the second is the port
-            proxyCommand = "bash -c \"${nc} $(${sed} -n '1p' ${ip-ahm.path}) $(${sed} -n '2p' ${ip-ahm.path})\"";
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-ahmad-hetzner-germany.path}) %p'";
+          };
+          parsa-iranserver-tehran = {
+            user = "root";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-parsa-iranserver-tehran.path}) %p'";
+          };
+          parsa-asiatech-tehran = {
+            user = "root";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-parsa-asiatech-tehran.path}) %p'";
+          };
+          ahmad-parspack-tehran = {
+            user = "root";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-ahmad-parspack-tehran.path}) %p'";
+          };
+          parsa-asiatech-tehran2 = {
+            user = "root";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-parsa-asiatech-tehran2.path}) %p'";
+          };
+          mohammad-do = {
+            user = "root";
+            checkHostIP = false;
+            proxyCommand = "bash -c '${nc} $(${cat} ${ip-mohammad-do.path}) %p'";
           };
         };
     };
