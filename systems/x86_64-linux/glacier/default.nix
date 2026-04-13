7e220580d0b504bf8f02c72cfb846d4141c14e06
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-04-13 11:39:49 +0330

    fix bluetooth not working in sleep

--- systems/x86_64-linux/glacier/default.nix
+++ systems/x86_64-linux/glacier/default.nix
@@ -20,4 +20,8 @@
   boot.extraModprobeConfig = ''
     snd_hda_intel power_save=0
   '';
+
+  # Use s2idle instead of S3 deep sleep so Bluetooth controller stays
+  # powered enough to receive wake signals from BT keyboard
+  boot.kernelParams = [ "mem_sleep_default=s2idle" ];
 }
