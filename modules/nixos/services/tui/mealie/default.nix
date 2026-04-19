fb90622e306c52418fdea95a2345f86c63c91505
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-04-19 09:27:21 +0330

    add mealie

---
+++ modules/nixos/services/tui/mealie/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "mealie";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "tui"
+      "service"
+      "automation"
+      "server"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = [
+      config.services.mealie.port
+    ];
+
+    services.mealie = {
+      enable = true;
+    };
+  };
+}
