fe4884afd080eaee4089285dfdc341b3b1622ccc
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-04-22 17:13:23 +0330

    set nix proxy for glacier

--- systems/x86_64-linux/glacier/default.nix
+++ systems/x86_64-linux/glacier/default.nix
@@ -16,6 +16,11 @@
     systemd-boot.enable = false;
   };
 
+  systemd.services.nix-daemon.environment = {
+    http_proxy = "socks5://127.0.0.1:10808";
+    https_proxy = "socks5://127.0.0.1:10808";
+    NIX_CURL_FLAGS = "-x socks5://127.0.0.1:10808";
+  };
   # Disable speaker buzzing sound
   boot.extraModprobeConfig = ''
     snd_hda_intel power_save=0
