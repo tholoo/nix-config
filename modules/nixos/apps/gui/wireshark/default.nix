8e630f44105871961fb3ac9e24eda290a80ea4a1
Ali Mohammadzadeh <ali0mhmz@gmail.com>
2026-04-22 10:05:38 +0330

    add wireshark

---
+++ modules/nixos/apps/gui/wireshark/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.mine) mkEnable;
+  cfg = config.mine.${name};
+  name = "wireshark";
+in
+{
+  options.mine.${name} = mkEnable config {
+    tags = [
+      "gui"
+      "gui-misc"
+      "network"
+    ];
+  };
+
+  config = mkIf cfg.enable {
+    programs.wireshark = {
+      enable = true;
+      usbmon = true;
+    };
+  };
+}
